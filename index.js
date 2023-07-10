26367193295f2d85f07dda4c98efcef70b04c027
janine65 <janine.franken@gmail.com>
2023-07-10 19:46:09 +0200

    express grösse erhöht

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ const corsOptionsDelegate = function (req, callback) {
   callback(null, corsOptions) // callback expects two parameters: error and options
 }
 
-app.use(express.text());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.text({limit: '25mb'}));
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({extended: true, limit: '25mb'}));
 
 app.use(cors(corsOptionsDelegate))
 app.use(function (req, res, next) {
